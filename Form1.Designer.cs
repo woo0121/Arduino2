74957f5e0155196f983c6fcd561ab3da5cc244e5
leejunho <junho8296@naver.com>
2023-09-04 09:29:39 +0900

    1

---
+++ Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace _01BASIC
+{
+    partial class winform
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PortNumber = new System.Windows.Forms.ComboBox();
+            this.conn_btn = new System.Windows.Forms.Button();
+            this.textArea = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.TEMP_BOX = new System.Windows.Forms.TextBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.sun_txt = new System.Windows.Forms.TextBox();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.DIS_TXT = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox6.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PortNumber
+            // 
+            this.PortNumber.Cursor = System.Windows.Forms.Cursors.Default;
+            this.PortNumber.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.PortNumber.FormattingEnabled = true;
+            this.PortNumber.Items.AddRange(new object[] {
+            "COM1",
+            "COM2",
+            "COM3",
+            "COM4",
+            "COM5",
+            "COM6",
+            "COM7",
+            "COM8"});
+            this.PortNumber.Location = new System.Drawing.Point(6, 20);
+            this.PortNumber.Name = "PortNumber";
+            this.PortNumber.Size = new System.Drawing.Size(78, 20);
+            this.PortNumber.TabIndex = 0;
+            this.PortNumber.SelectedIndexChanged += new System.EventHandler(this.PortNumber_SelectedIndexChanged);
+            // 
+            // conn_btn
+            // 
+            this.conn_btn.Location = new System.Drawing.Point(105, 20);
+            this.conn_btn.Name = "conn_btn";
+            this.conn_btn.Size = new System.Drawing.Size(75, 23);
+            this.conn_btn.TabIndex = 1;
+            this.conn_btn.Text = "연결";
+            this.conn_btn.UseVisualStyleBackColor = true;
+            this.conn_btn.Click += new System.EventHandler(this.conn_btn_Click);
+            // 
+            // textArea
+            // 
+            this.textArea.Location = new System.Drawing.Point(12, 29);
+            this.textArea.Multiline = true;
+            this.textArea.Name = "textArea";
+            this.textArea.ReadOnly = true;
+            this.textArea.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textArea.Size = new System.Drawing.Size(176, 124);
+            this.textArea.TabIndex = 2;
+            this.textArea.TextChanged += new System.EventHandler(this.textArea_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.PortNumber);
+            this.groupBox1.Controls.Add(this.conn_btn);
+            this.groupBox1.Location = new System.Drawing.Point(21, 32);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(197, 58);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Location = new System.Drawing.Point(21, 106);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "LED 제어";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(105, 29);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 52);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Off";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "On";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textArea);
+            this.groupBox3.Location = new System.Drawing.Point(252, 32);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 174);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "로그 확인";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.TEMP_BOX);
+            this.groupBox4.Location = new System.Drawing.Point(21, 213);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(200, 51);
+            this.groupBox4.TabIndex = 6;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "온도 센서";
+            // 
+            // TEMP_BOX
+            // 
+            this.TEMP_BOX.Enabled = false;
+            this.TEMP_BOX.Location = new System.Drawing.Point(9, 21);
+            this.TEMP_BOX.Name = "TEMP_BOX";
+            this.TEMP_BOX.Size = new System.Drawing.Size(171, 21);
+            this.TEMP_BOX.TabIndex = 0;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.sun_txt);
+            this.groupBox5.Location = new System.Drawing.Point(21, 281);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(200, 43);
+            this.groupBox5.TabIndex = 7;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "groupBox5";
+            // 
+            // sun_txt
+            // 
+            this.sun_txt.Enabled = false;
+            this.sun_txt.Location = new System.Drawing.Point(9, 16);
+            this.sun_txt.Name = "sun_txt";
+            this.sun_txt.Size = new System.Drawing.Size(171, 21);
+            this.sun_txt.TabIndex = 0;
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.DIS_TXT);
+            this.groupBox6.Location = new System.Drawing.Point(261, 213);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(200, 51);
+            this.groupBox6.TabIndex = 7;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "온도 센서";
+            // 
+            // DIS_TXT
+            // 
+            this.DIS_TXT.Enabled = false;
+            this.DIS_TXT.Location = new System.Drawing.Point(9, 21);
+            this.DIS_TXT.Name = "DIS_TXT";
+            this.DIS_TXT.Size = new System.Drawing.Size(171, 21);
+            this.DIS_TXT.TabIndex = 0;
+            // 
+            // winform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 361);
+            this.Controls.Add(this.groupBox6);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "winform";
+            this.Text = "WINFORMTEST";
+            this.Load += new System.EventHandler(this.winform_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.groupBox6.ResumeLayout(false);
+            this.groupBox6.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox PortNumber;
+        private System.Windows.Forms.Button conn_btn;
+        private System.Windows.Forms.TextBox textArea;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox TEMP_BOX;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.TextBox sun_txt;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.TextBox DIS_TXT;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace _01BASIC
+{
+    public partial class winform : Form
+    {
+
+        private SerialPort serialPort = new SerialPort();
+
+        public winform()
+        {
+            InitializeComponent();
+        }
+
+        private void PortNumber_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Console.WriteLine("HELLO WORLD");
+            //Console.WriteLine("sender : " + sender);
+            //Console.WriteLine("EventArgs : " + e);
+            ComboBox cb = (ComboBox)sender;
+            Console.Write("Selected Idx : " + cb.SelectedIndex+"  ");
+            Console.WriteLine("Selected Value : " + cb.Items[cb.SelectedIndex]);
+
+        }
+
+        private void conn_btn_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Conn_Btn Click : " + this.PortNumber.Items[this.PortNumber.SelectedIndex].ToString()); ;
+            try
+            {
+                this.serialPort.PortName = this.PortNumber.Items[this.PortNumber.SelectedIndex].ToString();
+                this.serialPort.BaudRate = 9600;
+                this.serialPort.DataBits = 8;
+                this.serialPort.StopBits = System.IO.Ports.StopBits.One;
+                this.serialPort.Parity = System.IO.Ports.Parity.None;
+                this.serialPort.Open();
+                Console.WriteLine("CONNECTION SUCCESS");
+                this.textArea.AppendText("Connected...\r\n");
+                this.serialPort.DataReceived+= new SerialDataReceivedEventHandler(SerialPort_DataReceived);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+                this.serialPort.Close();
+                this.textArea.AppendText("Fail..."+ex+"\r\n");
+            }
+        }
+        private void SerialPort_DataReceived(object sender, EventArgs e)
+        {
+            String recvData = this.serialPort.ReadLine();
+
+
+            // 스레드 생성 코드 
+            // Invoke(new Action( ()=> { /*처리로직*/ } ) );
+
+            /* LED 점등 유무 확인 스레드 */
+            if (recvData.StartsWith("LED:"))
+            {
+                Invoke(new Action(() => { Console.WriteLine(recvData); this.textArea.AppendText(recvData + "\r\n"); }));
+            }
+
+            /* 온도 센서  확인 스레드 */
+            if (recvData.StartsWith("TEMP: "))
+            {
+                Invoke(new Action(() => { this.TEMP_BOX.Text = ""; this.TEMP_BOX.Text = recvData.Replace("TEMP:", ""); }));
+            }
+            if (recvData.StartsWith("SUN: "))
+            {
+                Invoke(new Action(() => { this.sun_txt.Text = ""; this.sun_txt.Text = recvData.Replace("SUN:", ""); }));
+            }
+            if (recvData.StartsWith("DIS: "))
+            {
+                Invoke(new Action(() => { this.DIS_TXT.Text = ""; this.DIS_TXT.Text = recvData.Replace("DIS:", ""); }));
+            }
+        }
+        private void textArea_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void winform_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            serialPort.Write("1");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            serialPort.Write("0");
+        }
+    }
+}
